77a38823197e18e9e4adbe9fd23eeee211051b38
Ngoc Bich <ngocbich.892k1@gmail.com>
2025-09-13 19:39:00 +0700

    Update java_bootstrap.py

--- java_bootstrap.py
+++ java_bootstrap.py
@@ -1,11 +1,4 @@
-import os
-import subprocess
+import java_bootstrap
+java_bootstrap.ensure_java()
 
-def ensure_java():
-    try:
-        java_home = subprocess.check_output(["readlink", "-f", "/usr/bin/java"]).decode().strip()
-        java_home = "/".join(java_home.split("/")[:-2])
-        os.environ["JAVA_HOME"] = java_home
-        print(f"JAVA_HOME set to: {java_home}")
-    except Exception as e:
-        print("Could not set JAVA_HOME:", e)
+from pyspark.sql import SparkSession
